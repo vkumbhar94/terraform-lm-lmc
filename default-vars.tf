cf0e2558a60add4b86edfe57578bc61d22fdc984
Vaibhav Kumbhar <vkumbhar94@gmail.com>
2022-09-30 15:32:40 +0530

    Create default-vars.tf

---
+++ default-vars.tf
@@ -0,0 +1,26 @@
+variable "namespace" {
+  default = "logicmonitor"
+  type    = string
+}
+
+variable "reuse_values" {
+  default = true
+  type    = bool
+}
+
+variable "timeout" {
+  // increased timeout to 10 minutes since collector may take time to come up sometimes.
+  default = 600
+  type    = number
+}
+variable "lm_container_version" {
+  type = string
+}
+variable "repository" {
+  default = "https://logicmonitor.github.io/helm-charts"
+  type    = string
+}
+variable "name" {
+  default = "lm-container"
+  type    = string
+}
