859b0685f1e0a3fd8586e21d4cf9a8566c15e16b
vkumbhar94 <vkumbhar94@users.noreply.github.com>
2022-09-30 09:27:22 +0000

    release(*): releasing new version

--- variables.tf
+++ variables.tf
@@ -1,58 +1,7 @@
 variable "lm-container-configuration" {
   type = object({
-    global = object({
-      accessID  = optional(string)
-      accessKey = optional(string)
-      account   = optional(string)
-    })
-    argus = object({
-      accessID    = optional(string)
-      accessKey   = optional(string)
-      account     = optional(string)
-      clusterName = string
-    })
-    collectorset-controller = optional(object({
-      accessID  = optional(string)
-      accessKey = optional(string)
-      account   = optional(string)
-    }))
-    kube-state-metrics = optional(object({
-      enabled = optional(string)
-    }))
-  })
-}
 
-locals {
-  lm-container-configuration = defaults(var.lm-container-configuration, {
-    kube-state-metrics = {
-      enabled = true
-    }
-  })
-}
-
-variable "namespace" {
-  default = "logicmonitor"
-  type    = string
-}
-
-variable "reuse_values" {
-  default = true
-  type    = bool
-}
-
-variable "timeout" {
-  // increased timeout to 5 minutes since collector may take time to come up sometimes.
-  default = 600
-  type    = number
-}
-variable "lm_container_version" {
-  type = string
-}
-variable "repository" {
-  default = "https://logicmonitor.github.io/helm-charts"
-  type    = string
-}
-variable "name" {
-  default = "lm-container"
-  type    = string
+    global = optional(any)
+    
+})
 }
